380070ccd66fde756e9be3a892a271e5a871c870
realdevvora <legitvoradev@gmail.com>
2025-12-31 00:47:38 -0500

    update index ts file

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,3 +3,8 @@ declare module '@pmndrs/assets/hdri/city.exr' {
   const url: string
   export default url
 }
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string }
+  export default classes
+}
